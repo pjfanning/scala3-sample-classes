ac24cca33a859215afcb063218462017a82c1201
PJ Fanning <pjfanning@users.noreply.github.com>
2021-03-28 16:04:53 +0100

    extend tests

--- src/main/scala/com/github/pjfanning/scala3/EnclosingObject.scala
+++ src/main/scala/com/github/pjfanning/scala3/EnclosingObject.scala
@@ -2,4 +2,6 @@ package com.github.pjfanning.scala3
 
 object EnclosingObject {
   class EnclosedScala3Class(name: String, desc: Option[String])
+
+  enum EnclosedColorEnum { case Red, Green, Blue }
 }
---
+++ src/test/scala/com/github/pjfanning/scala3/TastySpec.scala
@@ -0,0 +1,54 @@
+package com.github.pjfanning.scala3
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.must.Matchers
+
+import scala.annotation.tailrec
+
+class TastySpec extends AnyWordSpec with Matchers {
+  private val specClass = classOf[TastySpec]
+
+  "TastyUtil" must {
+    "support Scala3Class" in {
+      hasTastyFile(classOf[Scala3Class]) mustBe true
+    }
+    "support Scala3CaseClass" in {
+      hasTastyFile(classOf[Scala3CaseClass]) mustBe true
+    }
+    "support Scala3Object" in {
+      hasTastyFile(Scala3Object.getClass) mustBe true
+    }
+    "support Scala3CaseObject" in {
+      hasTastyFile(Scala3CaseObject.getClass) mustBe true
+    }
+    "support ColorEnum" in {
+      hasTastyFile(ColorEnum.getClass) mustBe true
+    }
+    "support JavaCompatibleColorEnum" in {
+      hasTastyFile(JavaCompatibleColorEnum.getClass) mustBe true
+    }
+    "support EnclosingObject.EnclosedScala3Class" in {
+      hasTastyFile(classOf[EnclosingObject.EnclosedScala3Class]) mustBe true
+    }
+    "support EnclosingObject.EnclosedColorEnum" in {
+      hasTastyFile(EnclosingObject.EnclosedColorEnum.getClass) mustBe true
+    }
+    "not support Java class" in {
+      hasTastyFile(classOf[String]) mustBe false
+    }
+  }
+
+  @tailrec
+  private def hasTastyFile(clz: Class[_]): Boolean = {
+    clz != null && {
+      val baseName = clz.getCanonicalName.replace(".", "/")
+      val classFileBase = if (baseName.endsWith("$")) {
+        baseName.substring(0, baseName.length - 1)
+      } else {
+        baseName
+      }
+      val tastyFile = s"/$classFileBase.tasty"
+      Option(specClass.getResource(tastyFile)).isDefined || hasTastyFile(clz.getEnclosingClass)
+    }
+  }
+}
